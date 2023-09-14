ac626a3f59cf63ca020f35ad3d8649f1d05570c4
Av-sek <abhishekghorashainee07@gmail.com>
2023-09-14 19:28:24 +0545

    mongo db connection

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,20 @@ import logger from 'jet-logger';
 import EnvVars from '@src/constants/EnvVars';
 import server from './server';
 
+//import database connection
+import { database_client } from './database/connetion';
 
-// **** Run **** //
+// **** Connection with database on run **** //
+async function run() {
+  try {
+    await database_client.connect();
+    await database_client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await database_client.close();
+  }
+}
+run().catch(console.dir);
 
 const SERVER_START_MSG = ('Express server started on port: ' + 
   EnvVars.Port.toString());
