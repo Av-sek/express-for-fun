a96dcfd957da0e2cab39b06696fba87639638d28
Av-sek <abhishekghorashainee07@gmail.com>
2023-09-15 19:55:53 +0545

    cleaning up

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import jetValidator from 'jet-validator';
 
 import Paths from '../constants/Paths';
-import User from '@src/models/User';
 import UserRoutes from './UserRoutes';
 
 
@@ -17,31 +16,7 @@ const apiRouter = Router(),
 const userRouter = Router();
 
 // Get all users
-userRouter.get(
-  Paths.Users.Get,
-  UserRoutes.getAll,
-);
 
-// Add one user
-userRouter.post(
-  Paths.Users.Add,
-  validate(['user', User.isUser]),
-  UserRoutes.add,
-);
-
-// Update one user
-userRouter.put(
-  Paths.Users.Update,
-  validate(['user', User.isUser]),
-  UserRoutes.update,
-);
-
-// Delete one user
-userRouter.delete(
-  Paths.Users.Delete,
-  validate(['id', 'number', 'params']),
-  UserRoutes.delete,
-);
 
 // Add UserRouter
 apiRouter.use(Paths.Users.Base, userRouter);
