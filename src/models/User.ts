a6e06e5682868d7db89e7627e29c9600f231180e
Av-sek <abhishekghorashainee07@gmail.com>
2023-09-17 21:11:58 +0545

    adding role to the user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
 import { Schema, Document, Model } from "mongoose";
-export interface IUser{
+export interface IUser {
   username: string;
   email: string;
-  authentication: {
-    password: string;
-    salt: string;
-  };
+  password: string;
+  role: string;
   created_at: Date;
   updated_at: Date;
 }
 
 const UserSchema = new Schema({
-  username: {type:String, required: false, unique: true},
-  email: {type:String, required: false, unique: true},
-  authentication: {
-    password: {type:String, required: true, select: false},
-    salt: {type:String, required: true, select: false},
+  username: { type: String, required: false, unique: true },
+  email: { type: String, required: false, unique: true },
+  password: { type: String, required: false },
+  role: {
+    type: String,
+    required: false,
+    enum: ["admin", "user"],
+    default: "user",
   },
-  created_at: {type:Date, default: Date.now},
-  updated_at: {type:Date, default: Date.now},
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 });
 
-export const UserModel: Model<IUser> = mongoose.model<IUser>('User', UserSchema);
-
+export const UserModel: Model<IUser> = mongoose.model<IUser>(
+  "User",
+  UserSchema
+);
